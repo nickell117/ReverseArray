6254996ab26113d4fc6c940db2c49b83de67a49b
nickell117 <63254377+nickell117@users.noreply.github.com>
2020-04-28 08:42:49 -0600

    Add files via upload

---
+++ ReverseStringArray/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace ReverseStringArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please write something: ");
+            string input = Console.ReadLine();
+            Console.WriteLine(Reverse(input));
+        }
+
+        static string Reverse(string input)
+        {
+            char[] ara = input.ToCharArray();
+            Array.Reverse(ara);
+            return new string(ara);
+        }
+    }
+}
